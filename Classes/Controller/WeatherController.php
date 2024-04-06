d571b1b7efdda50b1e28b49de45148a15ac2de4e
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-04-06 19:51:01 +0200

    Refactor weather controller to  build the api link, retrieve data from openweathermap and assign the data to the fluid template

--- Classes/Controller/WeatherController.php
+++ Classes/Controller/WeatherController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Cyberelk\JheOpenweathermap\Controller;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
@@ -15,59 +14,20 @@ class WeatherController extends ActionController {
 	public function showAction() {
 
 		// //retrive data from ts settings
-		// $openweathermapApiKey = $this->settings['openweathermapApiKey'];
-		// $openWeatherMapApiUrl = $this->settings['openWeatherMapApiUrl'];
-		// $openWeatherMapApiCountry = $this->settings['openWeatherMapApiCountry'];
-		// $openWeatherMapApiLang = $this->settings['openWeatherMapApiLang'];
-		// $openWeatherMapApiUnits = $this->settings['openWeatherMapApiUnits'];
-
-		// //get fe_user data to show the correct local weather data
-		// if($GLOBALS['TSFE']->fe_user && $GLOBALS['TSFE']->fe_user->user['zip']){
-		// 	$currentZip = $GLOBALS['TSFE']->fe_user->user['zip'];
-		// } else {
-		// 	$currentZip = $this->settings['defaultZip'];
-		// }
-		// if($GLOBALS['TSFE']->fe_user && $GLOBALS['TSFE']->fe_user->user['city']){
-		// 	$currentCity = $GLOBALS['TSFE']->fe_user->user['city'];
-		// } else {
-		// 	$currentCity = $this->settings['defaultCity'];
-		// }
-
-		// $jsonArr = array(
-		// 	'apikey' => $openweathermapApiKey,
-		// 	'apiurl' => $openWeatherMapApiUrl,
-		// 	'apicountry' => $openWeatherMapApiCountry,
-		// 	'apilang' => $openWeatherMapApiLang,
-		// 	'apiunits' => $openWeatherMapApiUnits,
-		// 	'currentzip' => $currentZip,
-		// 	'currentcity' => $currentCity
-		// );
-
-		// $this->view->assign('weatherDataArr', base64_encode(serialize($jsonArr)));
-
-	}
-
-	public function sendRequestAction(){
-
-		return 'Willkommen in der richtigen Methode...';
-
-		$weatherDataArr = unserialize(base64_decode(GeneralUtility::_POST('data')));
-
-		$apikey = $weatherDataArr['apikey'];
-		$apiurl = $weatherDataArr['apiurl'];
-		$apicountry = $weatherDataArr['apicountry'];
-		$apilang = $weatherDataArr['apilang'];
-		$apiunits = $weatherDataArr['apiunits'];
-		$currentzip = $weatherDataArr['currentzip'];
-		$currentcity = $weatherDataArr['currentcity'];
-
-		$returnData = array();
+		$apikey = $this->settings['openweathermapApiKey'];
+		$apiurl = $this->settings['openWeatherMapApiUrl'];
+		$apicountry = $this->settings['openWeatherMapApiCountry'];
+		$apilang = $this->settings['openWeatherMapApiLang'];
+		$apiunits = $this->settings['openWeatherMapApiUnits'];
+		$currentzip = $this->settings['defaultZip'];
+		$currentcity = $this->settings['defaultCity'];
 
 		//assemble the openWeatherMap API link
 		$request = $apiurl . '?q=' . $currentzip . ',' . $apicountry . '&APPID=' . $apikey;
 
 		//retrieve the actual weather data from openWeatherMap API and decode the jason data
 		$response = file_get_contents($request);
+
 		$jsonObj = json_decode($response);
 
 		if($jsonObj->weather && $jsonObj->main && $jsonObj->sys){
@@ -86,44 +46,27 @@ class WeatherController extends ActionController {
 			}
 
 			//assemble return data array
-			$returnData['error'] = false;
-			$returnData['city'] = $currentcity;
-			$returnData['weather'] = $weather;
-			$returnData['temperature'] = $temperature;
-			$returnData['temperature_min'] = $temperature_min;
-			$returnData['temperature_max'] = $temperature_max;
-			$returnData['dayOrNight'] = $suffix;
-			$returnData['lang'] = $apilang;
+			$weatherData['error'] = false;
+			$weatherData['city'] = $currentcity;
+			$weatherData['weather'] = $weather;
+			$weatherData['temperature'] = $temperature;
+			$weatherData['temperature_min'] = $temperature_min;
+			$weatherData['temperature_max'] = $temperature_max;
+			$weatherData['dayOrNight'] = $suffix;
+			$weatherData['lang'] = $apilang;
 		} else {
-			$returnData['error'] = true;
-
-			// Deactivated the sending of the log data in error cases during my vacations
-			// Activate after return!
-			//$this->sendLogDataToAdmin($request, $response, $jsonObj);
+			$weatherData['error'] = true;
 		}
 
-		echo json_encode($returnData);
-		exit(0);
-	}
-
-	public function sendLogDataToAdmin($request, $response, $jsonObj){
-		$errorMailMessage = '
-			Request:' . $request . '
-			Response: ' . $response . '
-			Fehlermessage: '. $jsonObj->message . '
-			Fehlercode: ' . $jsonObj->cod;
-
-		mail('teampoint@bad-gmbh.de', 'Fehlermeldung api.openweathermap.org', $errorMailMessage);
+		$this->view->assign('weatherData', $weatherData);
 	}
 
 	public function convertKelvinToCelsius($temperature){
 		if(!is_numeric($temperature)){
 			return false;
 		}
-
 		$calculatedTemperature = round(($temperature - 273.15));
-
 		return $calculatedTemperature;
 	}
-
+	
 }
