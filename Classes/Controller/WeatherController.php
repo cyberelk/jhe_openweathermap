6c986ab077c1e611d070d5579e7f697835543d84
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-10-08 12:43:24 +0200

    Completely refactored the weather controller

--- Classes/Controller/WeatherController.php
+++ Classes/Controller/WeatherController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Cyberelk\JheOpenweathermap\Controller;
 
+use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 
@@ -13,119 +14,194 @@ use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 class WeatherController extends ActionController {
 
 	public function showAction() {
+		// Initialize weather data array with defaults to avoid undefined index errors.
+		$weatherData = [
+			'dataError' => false,
+			'error' => false,
+			'city' => '',
+			'weather' => [],
+			'temperature' => 0,
+			'temperature_min' => 0,
+			'temperature_max' => 0,
+			'dayOrNight' => '',
+			'lang' => ''
+		];
 
-		$dataError = false;
-		$apikey = '';
-		$apiurl = '';
-		$apilang = '';
-		$apiunits = '';
-		$apicountry = '';
-		$currentzip = '';
-		$currentcity = '';
-
-		// retrieve standard data from ts settings
-		if(isset($this->settings['openweathermapApiKey']) && $this->settings['openweathermapApiKey'] != ''
-		 && isset($this->settings['openWeatherMapApiUrl']) && $this->settings['openWeatherMapApiUrl'] != ''
-		 && isset($this->settings['openWeatherMapApiLang']) && $this->settings['openWeatherMapApiLang'] != ''
-		 && isset($this->settings['openWeatherMapApiUnits']) && $this->settings['openWeatherMapApiUnits'] != ''){
-			$apikey = $this->settings['openweathermapApiKey'];
-			$apiurl = $this->settings['openWeatherMapApiUrl'];
-			$apilang = $this->settings['openWeatherMapApiLang'];
-			$apiunits = $this->settings['openWeatherMapApiUnits'];
-		} else {
-			$dataError = true;
+		// Attempt to configure the API settings and load the weather data.
+		if ($this->configureApiSettings($weatherData)) {
+			$this->loadAndPrepareWeatherData($weatherData);
 		}
 
-		if(isset($this->settings['pluginType']) && $this->settings['pluginType'] == 'typoscript'){
-			if(isset($this->settings['openWeatherMapApiCountry']) && $this->settings['openWeatherMapApiCountry'] != ''
-			 && isset($this->settings['defaultZip']) && $this->settings['defaultZip'] != ''
-			 && isset($this->settings['defaultCity']) && $this->settings['defaultCity'] != ''){
-				$apicountry = $this->settings['openWeatherMapApiCountry'];
-				$currentzip = $this->settings['defaultZip'];
-				$currentcity = $this->settings['defaultCity'];
-			} else {
-				$dataError = true;
-			}
-		}
-
-		if(isset($this->settings['pluginType']) && $this->settings['pluginType'] == 'fe_user'){
-			// retrieve data from fe_user
-			/** @var FrontendUserAuthentication $fe_user */
-			$fe_user = $this->request->getAttribute('frontend.user');
-			if(!empty($fe_user)
-			 && isset($fe_user->user['country']) && $fe_user->user['country'] != ''
-			 && isset($fe_user->user['city']) && $fe_user->user['city'] != ''
-			 && isset($fe_user->user['zip']) && $fe_user->user['zip'] != ''){
-				 $apicountry = $fe_user->user['country'];
-				 $currentcity = $fe_user->user['city'];
-				 $currentzip = $fe_user->user['zip'];
-			 } else {
-				$dataError = true;
-			 }
-		}
-
-		if(isset($this->settings['pluginType']) && $this->settings['pluginType'] == 'plugin'){
-			if(isset($this->settings['country']) && $this->settings['country'] != ''
-			 && isset($this->settings['city']) && $this->settings['city'] != ''
-			 && isset($this->settings['zip']) && $this->settings['zip'] != ''){
-				 $apicountry = $this->settings['country'];
-				 $currentcity = $this->settings['city'];
-				 $currentzip = $this->settings['zip'];
-			 } else {
-				$dataError = true;
-			 }
-		}
-		
-		if($dataError === false) {
-			//assemble the openWeatherMap API link
-			$request = $apiurl . '?q=' . $currentzip . ',' . $apicountry . '&APPID=' . $apikey;
-
-			//retrieve the actual weather data from openWeatherMap API and decode the jason data
-			$response = file_get_contents($request);
-
-			$jsonObj = json_decode($response);
-
-			if($jsonObj->weather && $jsonObj->main && $jsonObj->sys){
-				//compile the openWeatherMap data for use in the fluid template
-				$weather = $jsonObj->weather; //array of possible data
-				$temperature = $this->convertKelvinToCelsius($jsonObj->main->temp);
-				$temperature_min = $this->convertKelvinToCelsius($jsonObj->main->temp_min);
-				$temperature_max = $this->convertKelvinToCelsius($jsonObj->main->temp_max);
-
-				$now = date('U'); //get current time
-
-				if($now > $jsonObj->sys->sunrise and $now < $jsonObj->sys->sunset){
-					$suffix = '-d';
-				}else{
-					$suffix = '-n';
-				}
-
-				//assemble return data array
-				$weatherData['dataError'] = false;
-				$weatherData['error'] = false;
-				$weatherData['city'] = $currentcity;
-				$weatherData['weather'] = $weather;
-				$weatherData['temperature'] = $temperature;
-				$weatherData['temperature_min'] = $temperature_min;
-				$weatherData['temperature_max'] = $temperature_max;
-				$weatherData['dayOrNight'] = $suffix;
-				$weatherData['lang'] = $apilang;
-			} else {
-				$weatherData['error'] = true;
-			}
-		} else {
-			$weatherData['dataError'] = true;
-		}
-
+		// Assign the prepared or defaulted weather data to the view.
 		$this->view->assign('weatherData', $weatherData);
 	}
 
-	public function convertKelvinToCelsius($temperature){
-		if(!is_numeric($temperature)){
+	private function configureApiSettings(&$weatherData) {
+		$settings = $this->initializeSettings();
+
+		if (!$this->applyStandardApiSettings($settings)) {
+			$weatherData['dataError'] = true;
 			return false;
 		}
-		$calculatedTemperature = round(($temperature - 273.15));
-		return $calculatedTemperature;
+
+		if (!$this->applyPluginSpecificSettings($settings)) {
+			$weatherData['dataError'] = true;
+			return false;
+		}
+
+		// Verify if all necessary settings have been defined.
+		if (in_array('', $settings, true)) {
+			$weatherData['dataError'] = true;
+			return false;
+		}
+
+		// Settings configured successfully, proceed with these settings.
+		$weatherData += $settings; // Merge settings into weather data array for future use.
+		return true;
+	}
+
+	private function initializeSettings() {
+		return [
+			'apikey' => '',
+			'apiurl' => '',
+			'apiversion' => '',
+			'apilang' => '',
+			'apiunits' => '',
+			'apicountry' => '',
+			'currentzip' => '',
+			'currentcity' => ''
+		];
+	}
+
+	private function applyStandardApiSettings(&$settings) {
+		$requiredApiSettings = [
+			'apikey' => 'openweathermapApiKey',
+			'apiurl' => 'openWeatherMapApiUrl',
+			'apiversion' => 'openweathermapApiVersion',
+			'apilang' => 'openWeatherMapApiLang',
+			'apiunits' => 'openWeatherMapApiUnits'
+		];
+
+		return $this->checkAndSetSettings($requiredApiSettings, $settings, $this->settings);
+	}
+
+	private function applyPluginSpecificSettings(&$settings) {
+		$pluginType = $this->settings['pluginType'] ?? '';
+		$pluginTypesSettingsMap = $this->getPluginTypesSettingsMap($pluginType);
+
+		if ($pluginType === 'fe_user') {
+			/** @var FrontendUserAuthentication $fe_user */
+			$fe_user = $this->request->getAttribute('frontend.user');
+			$source = $fe_user ? $fe_user->user : [];
+		} else {
+			$source = $this->settings;
+		}
+
+		// If pluginType is recognized, check and set the required settings.
+		if (array_key_exists($pluginType, $pluginTypesSettingsMap)) {
+			return $this->checkAndSetSettings($pluginTypesSettingsMap[$pluginType], $settings, $source);
+		}
+
+		// If pluginType is unrecognized, consider it a configuration error.
+		return false;
+	}
+
+	private function getPluginTypesSettingsMap($pluginType) {
+		return [
+			'typoscript' => [
+				'apicountry' => 'openWeatherMapApiCountry',
+				'currentzip' => 'defaultZip',
+				'currentcity' => 'defaultCity'
+			],
+			'fe_user' => [
+				'apicountry' => 'country',
+				'currentcity' => 'city',
+				'currentzip' => 'zip'
+			],
+			'plugin' => [
+				'apicountry' => 'country',
+				'currentcity' => 'city',
+				'currentzip' => 'zip'
+			]
+		];
+	}
+
+	private function loadAndPrepareWeatherData(&$weatherData) {
+		$apiResponse = $this->fetchWeatherDataFromApi($weatherData);
+
+		if (!$apiResponse || !$this->validateApiResponse($apiResponse)) {
+			$weatherData['error'] = true;
+			return;
+		}
+
+		// Populate weather data array with values derived from the API response.
+		$weatherData['city'] = $weatherData['currentcity'];
+		$weatherData['weather'] = $apiResponse->weather;
+		$weatherData['temperature'] = $this->convertKelvinToCelsius($apiResponse->main->temp);
+		$weatherData['temperature_min'] = $this->convertKelvinToCelsius($apiResponse->main->temp_min);
+		$weatherData['temperature_max'] = $this->convertKelvinToCelsius($apiResponse->main->temp_max);
+		$weatherData['dayOrNight'] = $this->getTimeOfDaySuffix($apiResponse);
+		$weatherData['lang'] = $weatherData['apilang'];
+	}
+
+	private function fetchWeatherDataFromApi($settings) {
+		// Determine the appropriate API request URL based on API version.
+		$requestUrl = $this->buildApiRequestUrl($settings);
+		if (empty($requestUrl)) {
+			return null;
+		}
+
+		$response = file_get_contents($requestUrl);
+		return json_decode($response);
+	}
+
+	private function validateApiResponse($apiResponse) {
+		return isset($apiResponse->weather, $apiResponse->main, $apiResponse->sys);
+	}
+
+	private function buildApiRequestUrl($settings) {
+		// Logic to construct API request URL based on the apiversion and other settings...
+		// Similar to the original snippet, but encapsulated within this method for clarity.
+		if($settings['apiversion'] == '2.5'){
+			$requestUrl = $settings['apiurl'] . $settings['apiversion'] . '/weather?q=' . $settings['currentzip'] . ',' . $settings['apicountry'] . '&APPID=' . $settings['apikey'];
+		}
+		if($settings['apiversion'] == '3.0'){
+			//TODO: Test this settings with api 3.0 which has to be paid for
+			$locationRequest = 'http://api.openweathermap.org/geo/1.0/zip?zip=13053,de&appid=' . $settings['apikey'];
+			$locationResponse = file_get_contents($locationRequest);
+			$locationsJsonObj = json_decode($locationResponse);
+			// https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}
+			$requestUrl = $settings['apiurl'] . $settings['apiversion'] . '/onecall?lat=' . $locationsJsonObj->lat . '&lon=' . $locationsJsonObj->lon . '&appid=' . $settings['apikey'];
+		}
+
+		return $requestUrl;
+	}
+
+	public function convertKelvinToCelsius($temperature) {
+		if (!is_numeric($temperature)) {
+			return false;
+		}
+		return round($temperature - 273.15);
+	}
+
+	public function checkAndSetSettings(array $requiredKeys, array &$settings, $source) {
+		foreach ($requiredKeys as $key => $settingKey) {
+			if (!isset($source[$settingKey]) || $source[$settingKey] == '') {
+				return false;
+			}
+			$settings[$key] = $source[$settingKey];
+		}
+		return true;
+	}
+
+	function getTimeOfDaySuffix($jsonObj) {
+		$now = time();
+
+		if ($now > $jsonObj->sys->sunrise && $now < $jsonObj->sys->sunset) {
+			return '-d'; // Daytime
+		} else {
+			return '-n'; // Nighttime
+		}
 	}
 
 }
