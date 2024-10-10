21472dc393e7d14074e2ffb9c14fab531935c7e4
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-10-10 12:53:07 +0200

    First TYPO3 v12 changes

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,3 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('jhe_openweathermap', 'Configuration/TypoScript', 'Open Weather Map');
--- ext_tables.php
+++ ext_tables.php
@@ -3,5 +3,3 @@
 defined('TYPO3') or die();
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-ExtensionManagementUtility::addStaticFile('jhe_openweathermap', 'Configuration/TypoScript', 'Open Weather Map');
